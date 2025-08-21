4596cf9a29cf4e9411cfc9d31ab624c176d6564d
Nokwanda Buthelezi <151032350+yamashenge@users.noreply.github.com>
2025-08-21 12:20:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 #from dotenv import load_dotenv
 
 # --- Load API Key from .env ---
-load_dotenv()
+#load_dotenv()
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 if not API_KEY:
     st.error("API key not found. Please set OPENROUTER_API_KEY in .env file.")
